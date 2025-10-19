eca2692d66dbd29bbb6bea7009dbac2b9c653981
Joe Vinten <joseph.vinten@gmail.com>
2025-10-19 14:37:01 +0100

    create server

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	const port = "8080"
+	mux := http.NewServeMux()
+
+	mux.Handle("/", http.FileServer(http.Dir(".")))
+	s := &http.Server{
+		Addr:    ":" + port,
+		Handler: mux,
+	}
+
+	log.Printf("Serving on port: %s\n", port)
+
+	log.Fatal(s.ListenAndServe())
+
+}
