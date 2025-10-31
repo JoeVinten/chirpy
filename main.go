6a846147b3a42fd201ee83885896c9cc21a8a62f
Joe Vinten <joseph.vinten@gmail.com>
2025-10-31 09:10:59 +0000

    add profanity filter

--- main.go
+++ main.go
@@ -1,15 +1,138 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
+	"slices"
+	"strings"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) writeRequests(w http.ResponseWriter, r *http.Request) {
+	template := fmt.Sprintf(`<html>
+		<body>
+			<h1>Welcome, Chirpy Admin</h1>
+			<p>Chirpy has been visited %d times!</p>
+		</body>
+		</html>`, cfg.fileserverHits.Load())
+	w.Header().Add("Content-Type", "text/plain;charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(template))
+
+}
+
+func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits.Store(0)
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Hits reset to 0"))
+}
+
+func handlerChirpsValidate(w http.ResponseWriter, r *http.Request) {
+	type parameters struct {
+		Body string `json:"body"`
+	}
+	type returnVals struct {
+		CleanBody string `json:"cleaned_body"`
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	params := parameters{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't decode parameters", err)
+	}
+
+	const maxChirpLength = 140
+
+	if len(params.Body) >= maxChirpLength {
+		respondWithError(w, http.StatusBadRequest, "Chirp is too long", nil)
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, returnVals{
+		CleanBody: profanityFilter(params.Body),
+	})
+}
+
+func profanityFilter(t string) string {
+	profanity := []string{"kerfuffle", "sharbert", "fornax"}
+	const filter = "****"
+
+	cleanBody := []string{}
+
+	for _, word := range strings.Split(t, " ") {
+		if slices.Contains(profanity, strings.ToLower(word)) {
+			cleanBody = append(cleanBody, filter)
+		} else {
+			cleanBody = append(cleanBody, word)
+		}
+	}
+
+	return strings.Join(cleanBody, " ")
+
+}
+
+func respondWithError(w http.ResponseWriter, code int, msg string, err error) {
+	if err != nil {
+		log.Println(err)
+	}
+	if code > 499 {
+		log.Printf("Responding with 5XX error: %s", err)
+	}
+	type errorValue struct {
+		Error string `json:"error"`
+	}
+	respondWithJSON(w, code, errorValue{
+		Error: msg,
+	})
+
+}
+
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+	w.Header().Set("Content-Type", "application/json")
+	dat, err := json.Marshal(payload)
+	if err != nil {
+		log.Printf("Error marshalling JSON: %s", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(code)
+	w.Write(dat)
+}
 
 func main() {
 	const port = "8080"
 	mux := http.NewServeMux()
 
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+	apiCfg := &apiConfig{}
+
+	mux.Handle("/app/", http.StripPrefix("/app/", apiCfg.middlewareMetricsInc(http.FileServer(http.Dir(".")))))
+
+	mux.HandleFunc("GET /admin/metrics", apiCfg.writeRequests)
+
+	mux.HandleFunc("POST /admin/reset", apiCfg.handlerReset)
+
+	mux.HandleFunc("GET /api/healthz", func(w http.ResponseWriter, r *http.Request) {
+		r.Header.Add("Content-Type", "text/plain;charset=utf-8")
+		w.WriteHeader(200)
+		w.Write([]byte("OK"))
+	})
+
+	mux.HandleFunc("POST /api/validate_chirp", handlerChirpsValidate)
+
 	s := &http.Server{
 		Addr:    ":" + port,
 		Handler: mux,
